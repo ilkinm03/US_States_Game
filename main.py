f808ba0715ef105334dbac0437ddfdbaa1b4e7f6
ilkinm03 <ilkinm2003@gmail.com>
2023-03-19 12:03:07 +0400

    write state name on the map if it is valid

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import turtle
 import pandas
 
+FONT = ("Arial", 8, "normal")
+
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+
+state_name = turtle.Turtle()
+state_name.hideturtle()
+state_name.penup()
 
 csv_data = "50_states.csv"
 states_data = pandas.read_csv(csv_data)
@@ -21,5 +27,9 @@ while game_is_on:
     ).title()
     if answer_state in states_list:
         counter += 1
+        x = states_data[states_data.state == answer_state]["x"]
+        y = states_data[states_data.state == answer_state]["y"]
+        state_name.goto(int(x), int(y))
+        state_name.write(answer_state, align="center", font=FONT)
 
 screen.exitonclick()
