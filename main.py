5f528df5d68707cb96391eac99745830227ef548
ilkinm03 <ilkinm2003@gmail.com>
2023-03-19 11:23:28 +0400

    read list of states from csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,5 +7,13 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-print(answer_state)
+csv_data = "50_states.csv"
+states_data = pandas.read_csv(csv_data)
+states_list = states_data["state"].to_list()
+
+answer_state = screen.textinput(
+    title="Guess the State",
+    prompt="What's another state's name?"
+).title()
+
+print(states_list)
